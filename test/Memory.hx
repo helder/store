d250456233e9adf12c1ece89bed70a6a88a29c9f
Ben Merckx <ben@codeurs.be>
2021-03-23 18:50:15 +0100

    Minor

--- test/Memory.hx
+++ test/Memory.hx
@@ -1,16 +1,15 @@
 package test;
 
 import helder.store.sqlite.SqliteStore;
+import helder.store.drivers.*;
 
 @:forward
 abstract Memory(SqliteStore) {
   public function new()
     this = new SqliteStore(
-      #if sqljs new helder.store.drivers.SqlJs(
-        new helder.store.drivers.SqlJs.Database()
-      )
-      #elseif js new helder.store.drivers.BetterSqlite3()
-      #elseif php new helder.store.drivers.Sqlite3()
+      #if sqljs new SqlJs(new SqlJs.Database())
+      #elseif js new BetterSqlite3()
+      #elseif php new Sqlite3()
       #else #error 'Not supported' #end
     );
 } 
