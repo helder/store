547ceda6f3674638db18a82a9867680f552ba95c
Ben Merckx <ben@codeurs.be>
2021-09-16 11:41:43 +0200

    Keep some types

--- scripts/CleanDist.hx
+++ scripts/CleanDist.hx
@@ -7,6 +7,8 @@ using StringTools;
 final esm = './dist/esm';
 final cjs = './dist/cjs';
 
+final keep = ['OrderBy', 'Document', 'Map', 'StdTypes'];
+
 function main() {
   function clean(target: String) {
     final dir = Path.join([esm, target]);
@@ -17,6 +19,7 @@ function main() {
         removed += clean(Path.join([target, file])) ? 1 : 0;
       } else {
         final name = file.substr(0, file.indexOf('.'));
+        if (keep.indexOf(name) > -1) continue;
         if (!FileSystem.exists(
           Path.join([esm, target, name + '.js'])
         )) {
