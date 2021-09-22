c36e94ea545f66810d8263b408c064e5db37fe7b
Ben Merckx <ben@codeurs.be>
2021-09-22 18:32:04 +0200

    Deal with optional count argument

--- src/helder/store/sqlite/Functions.hx
+++ src/helder/store/sqlite/Functions.hx
@@ -199,7 +199,8 @@ class Functions {
 
   /** The count(X) function returns a count of the number of times that X is not NULL in a group. The count(*) function (with no arguments) returns the total number of rows in the group. */
   public static function count(?x:Expression<Dynamic>):Expression<Int> {
-    return new Expression(Call('count', [toExpr(x)]));
+    final params = if ((x: Null<Dynamic>) == null) [] else [toExpr(x)];
+    return new Expression(Call('count', params));
   }
 
   /** The group_concat() function returns a string which is the concatenation of all non-NULL values of X. If parameter Y is present then it is used as the separator between instances of X. A comma (",") is used as the separator if Y is omitted. The order of the concatenated elements is arbitrary. */
